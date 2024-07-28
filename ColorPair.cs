1419026b982f699eaca17077fd9dca1343d2947d
K.V.Adarsh <52235640+kvadarsh200@users.noreply.github.com>
2024-07-29 00:06:02 +0530

    Create ColorPair.cs

---
+++ ColorPair.cs
@@ -0,0 +1,15 @@
+using System.Drawing;
+
+namespace TelCo.ColorCoder
+{
+    internal class ColorPair
+    {
+        internal Color majorColor;
+        internal Color minorColor;
+
+        public override string ToString()
+        {
+            return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
+        }
+    }
+}
