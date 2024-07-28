0ebe79bddca00187cb810117384c6a01dab21eff
K.V.Adarsh <52235640+kvadarsh200@users.noreply.github.com>
2024-07-29 00:08:39 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,60 +6,6 @@ namespace TelCo.ColorCoder
 {
     class Program
     {
-        private static readonly Color[] colorMapMajor;
-        private static readonly Color[] colorMapMinor;
-
-        internal class ColorPair
-        {
-            internal Color majorColor;
-            internal Color minorColor;
-
-            public override string ToString()
-            {
-                return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
-            }
-        }
-
-        static Program()
-        {
-            colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
-            colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
-        }
-
-        public static ColorPair GetColorFromPairNumber(int pairNumber)
-        {
-            ValidatePairNumber(pairNumber);
-
-            int zeroBasedPairNumber = pairNumber - 1;
-            int majorIndex = zeroBasedPairNumber / colorMapMinor.Length;
-            int minorIndex = zeroBasedPairNumber % colorMapMinor.Length;
-
-            return new ColorPair() { majorColor = colorMapMajor[majorIndex], minorColor = colorMapMinor[minorIndex] };
-        }
-
-        public static int GetPairNumberFromColor(ColorPair pair)
-        {
-            int majorIndex = Array.IndexOf(colorMapMajor, pair.majorColor);
-            int minorIndex = Array.IndexOf(colorMapMinor, pair.minorColor);
-
-            if (majorIndex == -1 || minorIndex == -1)
-            {
-                throw new ArgumentException($"Unknown Colors: {pair}");
-            }
-
-            return (majorIndex * colorMapMinor.Length) + (minorIndex + 1);
-        }
-
-        private static void ValidatePairNumber(int pairNumber)
-        {
-            int maxPairNumber = colorMapMajor.Length * colorMapMinor.Length;
-            if (pairNumber < 1 || pairNumber > maxPairNumber)
-            {
-                throw new ArgumentOutOfRangeException(
-                    $"Argument PairNumber:{pairNumber} is outside the allowed range");
-            }
-        }
-
         private static void Main(string[] args)
         {
             TestGetColorFromPairNumber();
@@ -78,12 +24,14 @@ namespace TelCo.ColorCoder
 
             foreach (var test in tests)
             {
-                ColorPair result = GetColorFromPairNumber(test.pairNumber);
+                ColorPair result = ColorMapper.GetColorFromPairNumber(test.pairNumber);
                 Console.WriteLine($"[In]Pair Number: {test.pairNumber}, [Out] Colors: {result}");
                 Debug.Assert(result.majorColor == test.majorColor);
                 Debug.Assert(result.minorColor == test.minorColor);
             }
         }
+
+       
 
         private static void TestGetPairNumberFromColor()
         {
